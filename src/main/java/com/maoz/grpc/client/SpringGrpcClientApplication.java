9b73f8f96bc0dcf487b2552212cb5702cf0a23e8
fhluky <fhluky@gmail.com>
2019-04-04 14:38:26 +0700

    first commit

---
+++ src/main/java/com/maoz/grpc/client/SpringGrpcClientApplication.java
@@ -0,0 +1,13 @@
+package com.maoz.grpc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrpcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrpcClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maoz/grpc/client/SpringGrpcClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maoz.grpc.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringGrpcClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
